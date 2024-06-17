4c11e4ba5ca11c8524bd02746f4a0c375ebecbcf
UWILL18 <uwill18@wgu.edu>
2024-06-17 15:52:16 -0500

    resume adjustments
    adjusting resume

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -3,12 +3,49 @@ import React from 'react';
 import Image from 'next/image';
 import './HeroSection.css';
 import {TypeAnimation} from 'react-type-animation';
+//import resume from '../../public/resume/EasterResume.pdf'
+
+
+
+
 
 /**bg-clip + gradient makes the first line of text colorful 
  * had to arrange directory for the file
 */
 
+/**ChatGPT used DOM manipulation to create a hyperlink to the resume in
+ * my current directory
+ */
 const HeroSection = () => {
+
+// const resumeUrl ="http://localhost:3000/public/resume/EasterResume.pdf"
+// const downloadResume = (url)=>{
+//   fetch(url)
+//   .then((response)=> response.blob())
+//   .then((blob)=>{
+//     const blobUrl = window.URL.createObjectURL(new Blob([blob]));
+//     const fileName = url.split("/").pop();
+//     const aTag = document.createElement("a");
+//     aTag.href = url;
+//     aTag.setAttribute("download", fileName);
+//     document.body.appendChild(aTag);
+//     aTag.click();
+//     aTag.remove();
+//   })
+// }
+
+
+    // const downloadResume = () => {
+    //   const resumePath = resume; // Update with the correct path
+    //   const link = document.createElement('a');
+    //   link.href = resumePath;
+    //   link.download = 'Uri_Easter_Resume.pdf';
+    //   document.body.appendChild(link);
+    //   link.click();
+    //   console.log("I was clicked")
+    //   document.body.removeChild(link);
+    // };
+
   return (
     <section>
       <div
@@ -34,7 +71,6 @@ const HeroSection = () => {
       ]}
       wrapper="span"
       speed={5}
-      
       repeat={Infinity}
     />
           </h1>
@@ -45,19 +81,23 @@ const HeroSection = () => {
           <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-black'>Hire Me</button>
           <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border '>
             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
-            Download CV
+              <a 
+              download ="EasterResume.pdf"
+              href="EasterResume.pdf"> 
+                Download CV
+              </a>
             </span>
             </button>
         </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-          <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-          <Image 
-          src="/images/hero-image4.png"
-          alt="hero-image"
-          className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hero-image'
-          width={400}
-          height={400}/>
+          <div className='lg:w-[400px] lg:h-[400px] relative'>
+            <Image 
+            src="/images/hero-image4.png"
+            alt="hero-image"
+            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hero-image  w-[100%] h-[100%] bg-[#181818] rounded-full'
+            width={400}
+            height={400}/>
           </div>
           
 
@@ -67,4 +107,5 @@ const HeroSection = () => {
   )
 }
 
-export default HeroSection
+
+export default HeroSection;
