d2a149074ab2b3dd027454274fcd870e515ca53a
UWILL18 <30840725+Uwill18@users.noreply.github.com>
2025-03-27 10:42:20 -0500

    resume section

--- app/constants/index.js
+++ app/constants/index.js
@@ -3,22 +3,31 @@ export const experiences = [
       year: "March 2024 - Present",
       role: "Full Stack Developer",
       company: "A7",
-      description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+      description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. 
+      Implemented RESTful APIs and integrated with MongoDB databases. 
+      Collaborated with stakeholders to define project requirements and timelines.`,
       technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
     },
     {
-      year: "2022 - 2023",
-      role: "Frontend Developer",
-      company: "Adobe",
-      description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-      technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+      year: "July 2022 - December 2022",
+      role: "Full-Stack Developer in Training",
+      company: "Nucamp Coding Bootcamp",
+      description: `•	Designed and built front-end applications using React.js, leveraging Redux for state management.
+•	Developed backend services with Node.js, Express, and MongoDB, implementing RESTful APIs.
+•	Implemented secure authentication workflows using JWT and OAuth.
+•	Utilized Git for version control and collaborated in Agile development environments.
+`,
+      technologies: ["HTML", "CSS", "React"],
     },
     {
-      year: "2021 - 2022",
-      role: "Full Stack Developer",
-      company: "Facebook",
-      description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-      technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+      year: "May 2019 - July 2022",
+      role: "Customer Service Advocate",
+      company: "Blue Cross and Blue Shield of Texas",
+      description: `•	Analyzed and interpreted complex medical benefit information with precision to address customer inquiries effectively.
+•	Utilized multitasking abilities to navigate multiple systems simultaneously and provide timely and accurate assistance to callers.
+•	Enhanced client satisfaction through proactive problem-solving and clear communication of solutions while going above and beyond.
+ations using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+      technologies: ["", "", "", ""],
     },
     {
       year: "2020 - 2021",
